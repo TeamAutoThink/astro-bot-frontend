3ce8bc17aedded61d2eb07500dc03ec8ac953a2c
Abhishek Karia <abhishek.karia.007@gmail.comm>
2025-07-25 22:30:21 +0530

    removed ununsed package

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
 // src/constants.js
 const ENV = import.meta.env
 export const BASE_URL = 'http://localhost:3000';
-export const PLANATERY_POSITION = '/api/users/planatery-position';
 export const PREDECTION = '/prediction';
 export const GOOGLE_API_KEY = ENV.VITE_GOOGLE_API_KEY
 
